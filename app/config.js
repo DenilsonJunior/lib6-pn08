0b3249378e03866e92b9ebfe6dda07e6d7900b98
unknown <dehjr1990@gmail.com>
2024-11-29 22:09:54 -0300

    [INITIAL]

---
+++ app/config.js
@@ -0,0 +1,41 @@
+var config = {
+    salvarDados: true,
+    debug: false,
+    waterMark: false,
+    language: 'pt-br',
+    video: 'default',
+    lms: {
+        name: 'default'
+    },
+    acessibility: {
+        tools: true,
+        outlines: false,
+        vlibras: false,
+        customLibras: false
+    },
+    behaviors: {
+        adaptive: false,
+        width: 1920,
+        height: 1080,
+        fontSize: 30
+    },
+    modalVoltar: {
+        active: false,
+        msg: 'Você quer continuar de onde parou ou reiniciar o curso?',
+        yes: 'CONTINUAR',
+        no: 'REINICIAR',
+        color: '#0a698d'
+    },
+    pages: [{
+            uid: 'onepage',
+            src: 'onepage/index.html'
+        },
+        {
+            uid: 'onepage copy',
+            src: 'onepage copy/index.html'
+        }
+    ]
+};
+try {
+    module.exports = config;
+} catch (e) {}
